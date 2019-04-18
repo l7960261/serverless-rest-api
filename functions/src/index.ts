fcce360105a25f42564b0abee09c2f012a96711b
wilson <l7960261@gmail.com>
2019-04-18 15:27:27 +0800

    Add dayjs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as firebaseHelper from 'firebase-functions-helper';
 import serviceAccount from './serviceAccountKey.json';
+import dayjs from 'dayjs'
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as any),
@@ -18,42 +19,46 @@ main.use('/api/v1', app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 
-const contactsCollection = 'contacts';
+const formatTemplate = 'YYYY-MM-DD HH:mm:ss';
+const licensesCollection = 'licenses';
 
 export const webApi = functions.https.onRequest(main);
 
-// Add new contact
-app.post('/contacts', (req, res) => {
+// Add new license
+app.post('/licenses', (req, res) => {
+    var data = Object.assign({}, req.body);
+    data.createdAt = dayjs().add(8, 'hour').format(formatTemplate);
+    data.expiredAt = dayjs(data.createdAt).add(30, 'day').format(formatTemplate);
+
     firebaseHelper.firestore
-        .createNewDocument(db, contactsCollection, req.body)
-        .then(docRef => console.log(docRef.id));
-    res.send('Create a new contact');
+        .createNewDocument(db, licensesCollection, data)
+    res.send('Create a new license');
 })
 
-// Update new contact
-app.patch('/contacts/:contactId', (req, res) => {
+// Update new license
+app.patch('/licenses/:licenseId', (req, res) => {
     firebaseHelper.firestore
-        .updateDocument(db, contactsCollection, req.params.contactId, req.body);
-    res.send('Update a new contact');
+        .updateDocument(db, licensesCollection, req.params.licenseId, req.body);
+    res.send('Update a new license');
 })
 
-// View a contact
-app.get('/contacts/:contactId', (req, res) => {
+// View a license
+app.get('/licenses/:licenseId', (req, res) => {
     firebaseHelper.firestore
-        .getDocument(db, contactsCollection, req.params.contactId)
+        .getDocument(db, licensesCollection, req.params.licenseId)
         .then(doc => res.status(200).send(doc));
 })
 
-// View all contacts
-app.get('/contacts', (req, res) => {
+// View all licenses
+app.get('/licenses', (req, res) => {
     firebaseHelper.firestore
-        .backup(db, contactsCollection)
+        .backup(db, licensesCollection)
         .then(data => res.status(200).send(data))
 })
 
-// Delete a contact 
-app.delete('/contacts/:contactId', (req, res) => {
+// Delete a license 
+app.delete('/licenses/:licenseId', (req, res) => {
     firebaseHelper.firestore
-        .deleteDocument(db, contactsCollection, req.params.contactId);
-    res.send('Contact is deleted');
+        .deleteDocument(db, licensesCollection, req.params.licenseId);
+    res.send('License is deleted');
 })
