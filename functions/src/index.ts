97bed836d8c3de5b725458cc8d2c21932a1f5811
wilson <l7960261@gmail.com>
2019-04-19 21:40:10 +0800

    Restore 918c5fb5756b6b728e3c7d93614d77d284ecc270

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -67,22 +67,18 @@ app.delete('/licenses/:licenseId', (req, res) => {
 // Activate a license
 app.patch('/activation/:licenseId', (req, res) => {
     const licenseId = req.params.licenseId;
-    const email = req.body.email;
 
     return firebaseHelper.firestore
         .checkDocumentExists(db, licensesCollection, licenseId)
         .then(result => {
             const notExists = !result.exists;
             const doc = result.data || {};
-            const matchEmail = doc.emails.indexOf(email) >= 0;
-            const hasTokenNotMatchEmail = () => doc.token && !matchEmail;
+            const hasTokenButNotRegular = () => doc.token && !doc.regular;
 
             if (notExists) {
                 res.send({ message: `${licenseId} is not correct` });
-            } else if (hasTokenNotMatchEmail()) {
+            } else if (hasTokenButNotRegular()) {
                 res.send({ message: `${licenseId} activated already` });
-            } else if (doc.token && matchEmail) {
-                res.send({ data: doc.token });
             } else {
                 const token = uuidv1();
                 console.log(`Activate from Ip:${req.connection.remoteAddress} Token:${token}`);
